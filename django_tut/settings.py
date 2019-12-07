a15c3249463a4cf3ece41fab2ac8b1bf8982c023
Kushan Vora <kushpvo@gmail.com>
2019-12-07 09:55:18 +0530

    Added various env varriables

--- django_tut/settings.py
+++ django_tut/settings.py
@@ -20,10 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '=w6s_iuui-9tr%e)%640i0+ujt5jnelb7ekk4=j7$a93dw(**-'
+# SECRET_KEY = '=w6s_iuui-9tr%e)%640i0+ujt5jnelb7ekk4=j7$a93dw(**-'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = os.environ.get('DEBUG_VALUE')
+DEBUG = TRUE
 
 ALLOWED_HOSTS = ['django-tut-blog.herokuapp.com', ]
 
