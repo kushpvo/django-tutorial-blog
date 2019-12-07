b947102d667697111dc94f4b3a2b6dc9f90781d3
Kushan Vora <kushpvo@gmail.com>
2019-12-07 16:19:11 +0530

    Fixed typo

--- django_tut/settings.py
+++ django_tut/settings.py
@@ -25,8 +25,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
-DEBUG = True
+DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
+# DEBUG = True
 
 ALLOWED_HOSTS = ['django-tut-blog.herokuapp.com', ]
 
@@ -156,6 +156,6 @@ AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.s3boto3Storage'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3boto3Storage'
 
 django_heroku.settings(locals())
